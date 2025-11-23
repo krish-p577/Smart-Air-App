1935aea6c4062e37ca6f2ce079f30a7d0283c261
Raihan C <139701695+raihanCarder@users.noreply.github.com>
2025-11-23 00:19:37 -0500

    feat: finish glossary feature

--- app/src/main/java/com/SmartAir/glossary/view/GlossaryActivity.java
+++ app/src/main/java/com/SmartAir/glossary/view/GlossaryActivity.java
@@ -1,5 +1,28 @@
 package com.SmartAir.glossary.view;
 
-public class GlossaryActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.SmartAir.R;
 
+public class GlossaryActivity extends AppCompatActivity{
+
+    private Button exitBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glossary);
+
+        exitBtn = findViewById(R.id.glossaryExitBtn);
+
+        exitBtn.setOnClickListener(v -> {
+            exit();
+        });
+    }
+
+    public void exit(){
+        // TODO: Return back to whatever homepage
+    }
 }
+
