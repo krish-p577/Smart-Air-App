e27dc39d2b3e38cb87a44859bd323c56ba3f7fa1
Krish Patel <krishpatel8976@gmail.com>
2025-11-23 02:14:27 -0500

    feat: add spinner functionality to parent dashboard

--- app/src/main/java/com/SmartAir/ParentDashboard/view/ParentDashboardActivity.java
+++ app/src/main/java/com/SmartAir/ParentDashboard/view/ParentDashboardActivity.java
@@ -1,6 +1,7 @@
 package com.SmartAir.ParentDashboard.view;
 
 import android.annotation.SuppressLint;
+import android.hardware.biometrics.BiometricManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,12 +12,17 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.SmartAir.ParentDashboard.model.ChildModel;
 import com.SmartAir.ParentDashboard.model.ParentModel;
 import com.SmartAir.ParentDashboard.presenter.ParentDashboardPresenter;
 import com.SmartAir.R;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParentDashboardActivity extends AppCompatActivity {
 
@@ -42,54 +48,106 @@ public class ParentDashboardActivity extends AppCompatActivity {
 
         Spinner spinner = findViewById(R.id.mySpinner);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+        List<String> childList = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 this,
-                R.array.options_array,
-                android.R.layout.simple_spinner_item
+                android.R.layout.simple_spinner_item,
+                childList
         );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         spinner.setAdapter(adapter);
+        getUserChildren(adapter, childList);
+
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selected = parent.getItemAtPosition(position).toString();
-                Log.i("Spinner", "Selected: " + selected);
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedChild = childList.get(i);
+                dbTest(box1,selectedChild);
+
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
 
-
-
-        dbTest(box1);
 
 
 
     }
 
 
+    @SuppressLint("SetTextI18n")
+    protected void getUserChildren(ArrayAdapter<String> adapter, List<String> childList){
+        Log.i("DEBUG", "Get Children");
+        db.collection("users").document("1")
+                .collection("children")
+                .get().
+                addOnSuccessListener(queryDocumentSnapshots -> {
+                   childList.clear();
+
+                   for (DocumentSnapshot document : queryDocumentSnapshots) {
+                       String childName = document.getString("name");
+                       if (childName != null) {
+                           childList.add(childName);
+                       }
+                   }
+
+                   adapter.notifyDataSetChanged();
+                   Log.i("SPINNER TAG", "Loaded Children" + childList);
+
+                }).addOnFailureListener(e ->{
+                    Log.e("SPINNER FAILUE", "ERR", e);
+                });
+
+    }
+
+//    @SuppressLint("SetTextI18n")
+//    protected void updateDashboard(String selectedChild, TextView box1){
+//        Log.i("DEBUG", "function initilize");
+//
+//        db.collection("users").
+//                document("1")
+//                .get()
+//                .addOnSuccessListener(documentSnapshot -> {
+//                    if (documentSnapshot.exists()) {
+//                        ParentModel user = documentSnapshot.toObject((ParentModel.class));
+//                        String name = documentSnapshot.getString("name");
+//                        String role = documentSnapshot.getString("role");
+//
+//                        assert user != null;
+//                        test_text.setText("Name: " + user.getName() + "Role: " + user.getRole());
+//
+//                        Log.i("DEBUG", "DEBUG NAME:" + name + "   " + role);
+//                    }
+//                })
+//                .addOnFailureListener(e ->{
+//
+//                });
+//
+//    }
+
 
     @SuppressLint("SetTextI18n")
-    protected void dbTest(TextView test_text){
-        Log.i("TAGGGG", "function initilize");
+    protected void dbTest(TextView test_text, String selectedChild){
+        Log.i("DEBUG", "function initilize");
 
-        db.collection("Users").
-                document("f7VfCpCK355148sX129J")
+        db.collection("users").
+                document("1")
                 .get()
                 .addOnSuccessListener(documentSnapshot -> {
                    if (documentSnapshot.exists()) {
                        ParentModel user = documentSnapshot.toObject((ParentModel.class));
                        String name = documentSnapshot.getString("name");
-                       String age = documentSnapshot.getString("role");
+                       String role = documentSnapshot.getString("role");
 
                        assert user != null;
-                       test_text.setText("Name: " + user.getName() + "Age: " + user.getRole());
+                       test_text.setText("Name: " + user.getName() + "Role: " + user.getRole());
 
-                       Log.i("TAG", "DEBUG NAME:" + name + "   " + age);
+                       Log.i("DEBUG", "DEBUG NAME:" + name + "   " + role);
                    }
                 })
                 .addOnFailureListener(e ->{
@@ -97,5 +155,7 @@ public class ParentDashboardActivity extends AppCompatActivity {
                 });
 
     }
+
+
 
 }
